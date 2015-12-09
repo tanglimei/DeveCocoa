4344e890556d3df2ac5c1eb933a16bd23bed0019
tanglimei <562630424@qq.com>
2015-12-09 14:45:58 +0800

    增加了一个View

---
+++ 12306DeveCocoa/DCView.h
@@ -0,0 +1,13 @@
+//
+//  DCView.h
+//  DeveCocoa
+//
+//  Created by 悠然天地 on 15/12/9.
+//  Copyright © 2015年 KuaiYong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DCView : UIView
+
+@end
---
+++ 12306DeveCocoa/DCView.m
@@ -0,0 +1,20 @@
+//
+//  DCView.m
+//  DeveCocoa
+//
+//  Created by 悠然天地 on 15/12/9.
+//  Copyright © 2015年 KuaiYong. All rights reserved.
+//
+
+#import "DCView.h"
+
+@implementation DCView
+
+-(instancetype)init{
+    if (self = [super init]) {
+        NSLog(@"init");
+    }
+    return self;
+}
+
+@end
